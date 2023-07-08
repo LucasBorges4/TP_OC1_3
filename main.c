a137eb16988d9b406b43763099390a6e394273e3
LucasBorges4 <lucasborges-2013@hotmail.com>
2023-07-08 14:14:18 -0300

    Arrumando as funções no main

--- main.c
+++ main.c
@@ -1,9 +1,40 @@
 #include "arquivos.h"
-#define TAM 10
+#define TAM 20
 
-short atribui_vetor(short vetor[], short tamanho){
-    short *p, i = 0;
-    short limite_superior;
+//atribui e mostra;
+short atribui_vetor(int*, int);
+short mostra_vetor(int*, int);
+
+//bubbleSort;
+void troca(int*, int*);
+void bubbleSort(int*, int);
+
+int main(void){
+    srand(time(NULL)); //Semente para a rand, gerar numeros aleatorios com base no tempo;
+
+    int vetor1[TAM], vetor2[TAM];
+
+    atribui_vetor(vetor1, TAM);
+    atribui_vetor(vetor2, TAM);
+    
+    printf("Vetores Originais:\n");
+    mostra_vetor(vetor1, TAM);
+    mostra_vetor(vetor2, TAM);
+    
+    printf("Vetores Ordenados pelo bubblesort:\n");
+    bubbleSort(vetor1, TAM);
+    bubbleSort(vetor2, TAM);
+ 
+    mostra_vetor(vetor1, TAM);
+    mostra_vetor(vetor2, TAM);
+
+    printf("\n");
+    return 0;
+}
+
+short atribui_vetor(int vetor[], int tamanho){
+    int *p, i = 0;
+    int limite_superior;
     limite_superior = rand();
 
     if (vetor == NULL || tamanho == 0) return -1;
@@ -16,14 +47,36 @@ short atribui_vetor(short vetor[], short tamanho){
     return 0;
 }
 
-int main(void){
-    srand(time(NULL)); //Semente para a rand, gerar numeros aleatorios com base no tempo;
-
-    short vetor1[TAM], vetor2[TAM];
-
-    atribui_vetor(vetor1, TAM);
-    atribui_vetor(vetor2, TAM);
-
+short mostra_vetor(int vetor[], int tamanho){
+    if (vetor == NULL || tamanho == 0) return -1;
+    
+    for (short i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
+    }
     printf("\n");
-    return 0;
+
+    return 1;
+}
+
+void troca(int* xp, int* yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+  
+void bubbleSort(int vetor[], int tam_vetor){
+    int i, j;
+    bool trocou;
+    for (i = 0; i < tam_vetor - 1; i++) {
+        trocou = false;
+        for (j = 0; j < tam_vetor - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                troca(&vetor[j], &vetor[j + 1]);
+                trocou = true;
+            }
+        }
+        
+        if (trocou == false)
+            break;
+    }
 }
