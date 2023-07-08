769080413eb61b23e8773266d8cbb541efa080fd
LucasBorges4 <lucasborges-2013@hotmail.com>
2023-07-08 13:19:29 -0300

    Geração de numero aleatorio

--- main.c
+++ main.c
@@ -1,21 +1,28 @@
 #include "arquivos.h"
 #define TAM 10
 
-int atribui_vetor(int vetor[], int tamanho){
-    int *p, i = 0;
+short atribui_vetor(short vetor[], short tamanho){
+    short *p, i = 0;
+    short limite_superior;
+    limite_superior = rand();
+
+    if (vetor == NULL || tamanho == 0) return -1;
+
     for (p = &vetor[0]; p < &vetor[tamanho]; p++){
-        *p = 2*i;
+        *p = rand() % (limite_superior % 100); // retornar um valor pseudoaleatorio menor que 1000
         i++;
     }
+
+    return 0;
 }
 
 int main(void){
-    int vetor1[TAM], vetor2[TAM];
+    srand(time(NULL)); //Semente para a rand, gerar numeros aleatorios com base no tempo;
+
+    short vetor1[TAM], vetor2[TAM];
 
     atribui_vetor(vetor1, TAM);
     atribui_vetor(vetor2, TAM);
-
-    printf("Primeira posição do vetor: %d\n", vetor1[0]);
 
     printf("\n");
     return 0;
